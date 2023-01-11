68b8b40a3301d5562a6778c34991d1649ea15cbf
Joao Vitor <joaovitormoraveis@gmail.com>
2023-01-10 20:40:37 -0500

    Enabling Security and adding in memory users + roles

--- src/main/java/com/joao/springmvcwithsecurity/config/SpringWebConfig.java
+++ src/main/java/com/joao/springmvcwithsecurity/config/SpringWebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.joao.springmvcwithsecurity")
-public class SpringConfig {
+public class SpringWebConfig {
 
     @Bean
     public ViewResolver viewResolver() {
---
+++ src/main/java/com/joao/springmvcwithsecurity/config/SpringWebSecurityApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.joao.springmvcwithsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringWebSecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
