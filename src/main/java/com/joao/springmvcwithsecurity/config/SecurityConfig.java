17ceacbf91f46c1be68e54a90a5cfe59752f0394
Joao Vitor <joaovitormoraveis@gmail.com>
2023-01-16 21:32:21 -0500

    Configuraing datasource and implementing bean for read roles from db

--- src/main/java/com/joao/springmvcwithsecurity/config/SecurityConfig.java
+++ src/main/java/com/joao/springmvcwithsecurity/config/SecurityConfig.java
@@ -1,19 +1,49 @@
 package com.joao.springmvcwithsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private DataSource securityDataSource;
+
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+        return new JdbcUserDetailsManager(securityDataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests(configure ->
+                        configure.antMatchers("/css/**").permitAll()
+                                .antMatchers("/console/**").permitAll()
+                                .antMatchers("/").hasRole("EMPLOYEE")
+                                .antMatchers("/leaders/**").hasRole("MANAGER")
+                                .antMatchers("/systems/**").hasRole("ADMIN"))
+                .formLogin(configure ->
+                        configure.loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll())
+                .logout(LogoutConfigurer::permitAll)
+                .exceptionHandling(configure -> configure.accessDeniedPage("/access-denied"))
+                .build();
+    }
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
@@ -36,23 +66,6 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeRequests(configure ->
-                        configure.antMatchers("/css/**").permitAll()
-                                .antMatchers("/").hasRole("EMPLOYEE")
-                                .antMatchers("/leaders/**").hasRole("MANAGER")
-                                .antMatchers("/systems/**").hasRole("ADMIN"))
-                .formLogin(configure ->
-                        configure.loginPage("/showMyLoginPage")
-                                .loginProcessingUrl("/authenticateTheUser")
-                                .permitAll())
-                .logout(LogoutConfigurer::permitAll)
-                .exceptionHandling(configure -> configure.accessDeniedPage("/access-denied"))
-                .build();
-    }
+    }*/
 
 }
