e9972045661029979570640e5ac9272a3a4d6b0f
Joao Vitor <joaovitormoraveis@gmail.com>
2023-01-23 22:14:09 -0500

    Disabling csrf and frameOptions to allow using h2databse (for now)

--- src/main/java/com/joao/springmvcwithsecurity/config/SecurityConfig.java
+++ src/main/java/com/joao/springmvcwithsecurity/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
@@ -28,6 +30,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers().frameOptions().disable()
+                .and()
                 .authorizeRequests(configure ->
                         configure.antMatchers("/css/**").permitAll()
                                 .antMatchers("/console/**").permitAll()
